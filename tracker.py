4f9d044d46e86a889b20c8ad8be72f8654cc85a8
Martin <martin1hartmann@web.de>
2026-05-04 14:33:31 +0200

    phase A: erste version mit 4 Abfragen

---
+++ tracker.py
@@ -0,0 +1,14 @@
+print("Practice Tracker")
+print("----------------")
+
+
+meditation = input("Hast du gestern meditiert?")
+kraftsport = input("Kraftsport? Y/N?")
+sauna = input("Sauna? Y/N?")
+coding = input("Coding?Y/N")
+
+print()
+print(f"Du hast geantwortet:{meditation}")
+print(f"Kraftsport:{kraftsport}")
+print(f"Sauna:{sauna}")
+print(f"Coding:{coding}")
