bea6e5425501b3953fe82d51e8652024b83b1e77
Martin <martin1hartmann@web.de>
2026-05-05 11:35:24 +0200

    phase A: abgeschlossen: Json - doubletten check

--- tracker.py
+++ tracker.py
@@ -1,14 +1,42 @@
+import json
+from datetime import date
+
+heute = date.today().isoformat()
+
+try: 
+    with open("entries.json", "r") as f:
+        eintraege = json.load(f)
+except (FileNotFoundError, json.JSONDecodeError):
+    eintraege = {}
+
+if heute in eintraege:
+    print("Heute gibt es schon einen Eintrag.")
+    exit()
+
+print("Heute gibt es noch keinen Eintrag!")
 print("Practice Tracker")
 print("----------------")
-
 
 meditation = input("Hast du gestern meditiert?")
 kraftsport = input("Kraftsport? Y/N?")
 sauna = input("Sauna? Y/N?")
-coding = input("Coding?Y/N")
+coding = input("Coding?Y/N?")
 
 print()
 print(f"Du hast geantwortet:{meditation}")
 print(f"Kraftsport:{kraftsport}")
 print(f"Sauna:{sauna}")
 print(f"Coding:{coding}")
+
+eintraege[heute] = {
+    "meditation": meditation,
+    "kraftsport": kraftsport,
+    "sauna": sauna,
+    "coding": coding
+}
+
+with open("entries.json", "w") as f: 
+    json.dump(eintraege, f,indent=4)
+
+
+print(f"Eintrag für {heute} gespeichert.")
