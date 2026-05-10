c53239b6847bfed1ee2d673775fd622ed1197855
Martin <martin1hartmann@web.de>
2026-05-10 12:03:29 +0200

    phase B: tracker.py refactored in drei funktionen

--- tracker.py
+++ tracker.py
@@ -6,33 +6,45 @@ console = Console()
 
 heute = date.today().isoformat()
 
-try: 
-    with open("entries.json", "r") as f:
-        eintraege = json.load(f)
-except (FileNotFoundError, json.JSONDecodeError):
-    eintraege = {}
+def lade_eintraege():
+    try: 
+        with open("entries.json", "r") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+    
+def speichere_eintraege(daten):
+    with open("entries.json", "w") as f: 
+        json.dump(daten, f,indent=4)
 
-table = Table(title="Habit-Tracker")
-table.add_column("Date", justify="right", style="cyan", no_wrap=True)
-table.add_column("Kraftsport", style="magenta")
-table.add_column("Meditation", justify="right", style="green")
-table.add_column("Sauna", justify="right", style="green")
-table.add_column("Coding", justify="right", style="green")
+def zeige_uebersicht(datensatz: dict)-> None:
+    table = Table(title="Habit-Tracker")
+    table.add_column("Date", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Kraftsport", style="magenta")
+    table.add_column("Meditation", justify="right", style="green")
+    table.add_column("Sauna", justify="right", style="green")
+    table.add_column("Coding", justify="right", style="green")
 
-tage = []
+    tage = []
 
-for n in range(7):
-    tag = date.today() - timedelta(days=n)
-    tage.append(tag.isoformat())
+    for n in range(7):
+        tag = date.today() - timedelta(days=n)
+        tage.append(tag.isoformat())
 
-for tag in tage:
-    if tag in eintraege:
-        e = eintraege[tag]
-        table.add_row(tag, e["kraftsport"], e["meditation"], e["sauna"], e["coding"] )
-    else:
-        table.add_row(tag, "-", "-", "-", "-")
+    for tag in tage:
+        if tag in datensatz:
+            e = datensatz[tag]
+            table.add_row(tag, e["kraftsport"], e["meditation"], e["sauna"], e["coding"] )
+        else:
+            table.add_row(tag, "-", "-", "-", "-")
 
-console.print(table)
+    console.print(table)
+
+
+
+eintraege = lade_eintraege()
+
+zeige_uebersicht(eintraege)
 
 if heute in eintraege:
     print("Heute gibt es schon einen Eintrag.")
@@ -60,8 +72,6 @@ eintraege[heute] = {
     "coding": coding
 }
 
-with open("entries.json", "w") as f: 
-    json.dump(eintraege, f,indent=4)
-
+speichere_eintraege(eintraege)
 
 print(f"Eintrag für {heute} gespeichert.")
